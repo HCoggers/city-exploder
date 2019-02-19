1b0181194dc9e1180c812e024135362c9a824353
HCoggers <harry.cogswell@gmail.com>
2019-02-19 11:47:04 -0800

    installs cors and removes public directory

--- server.js
+++ server.js
@@ -5,13 +5,39 @@ require('dotenv').config();
 
 // Load express to do the heavy lifting of the server
 const express = require('express');
-const app = express();
+const cors = require('cors');
 
 // Establish the PORT number
 const PORT = process.env.PORT || 3000;
 
+const app = express();
+app.use(cors());
+
+// API Paths
+app.get('/location', (request, response) => {
+  const locationData = searchToLatLong(request.query.data);
+  response.send(locationData);
+});
+
+// Turn the server on
+app.listen(PORT, () => console.log(`App is up on ${PORT}`));
+
 // Create callback functions for our app methods
-const Location = (query, res) => {
+
+function handleError(err, res) {
+  console.error(err);
+  if (res) res.status(500).send('Sorry, something went wrong');
+}
+
+
+function searchToLatLong(query) {
+  const geoData = require('./data/geo.json');
+  const location = new Location(query, geoData);
+  console.log('location in searchToLatLong()', location);
+  return location;
+}
+
+function Location(query, res) {
   console.log('res in Location', res);
   this.search_query = query;
   this.formatted_query = res.results[0].formatted_address;
@@ -19,29 +45,10 @@ const Location = (query, res) => {
   this.longitude = res.results[0].geometry.location.lng;
 }
 
-const searchToLatLong = (query) => {
-  const geoData = require('./data/geo.json');
-  const location = new Location(query, geoData);
-  console.log('location in searchToLatLong()', location);
-  return location;
-}
 
-const handleError = (err, res) => {
-  console.error(err);
-  if (res) res.status(500).send('Sorry, something went wrong');
-}
 
-// Tell express where to load our 'data' files from
-app.use(express.static('./data'));
 
-// Create paths that the user can access the server from
-app.get('/location', (request, response) => {
-  const locationData = searchToLatLong(request.query.data);
-  response.send(locationData);
-});
 
 // Add a catch-all to get routes that don't exist
 app.use('*', (request, response) => response.send('Sorry, that route does not exist'));
 
-// Turn the server on
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
